fa0e2b43351ffd646b073ebbfbe88361a48787ff
Manit <manit.manu4@gmail.com>
2025-03-08 13:40:59 +0530

    first 08-03

--- boot_intro/src/main/java/com/srivasavi/config/SecurityConfig.java
+++ boot_intro/src/main/java/com/srivasavi/config/SecurityConfig.java
@@ -3,19 +3,25 @@ package com.srivasavi.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.srivasavi.helper.JWTAuthenticationFilter;
 import com.srivasavi.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	JWTAuthenticationFilter authenticationFilter;
 	
 	@Autowired
 	CustomUserDetailsService customUserDetailsService;
@@ -27,17 +33,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable().cors().disable()
 		.authorizeRequests()			
 				//.antMatchers("/firstMessage", "/secondMessage", "/allUsers").permitAll()
-				.antMatchers("/public/*", "/student/create").permitAll()
-				.antMatchers("student/allStudents").hasRole("Employee")
+				.antMatchers("/student/create").permitAll()
+				.antMatchers("/student/allStudents").hasRole("EMPLOYEE")
+				.antMatchers("/student/getByName/**", "/public/firstMessage").hasRole("ADMIN")
+				.antMatchers("/token").permitAll()
 				//.antMatchers("/student/*").hasRole("Admin")
-				.anyRequest().authenticated().and().httpBasic();
+				.anyRequest().authenticated().and()//.httpBasic();
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+				http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 	}
 
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		System.out.println("configure()");
-//		auth.inMemoryAuthentication().withUser("xi").password(passwordEncoder().encode("123")).roles("Admin");
-//		auth.inMemoryAuthentication().withUser("jing").password(passwordEncoder().encode("234")).roles("Employee");
+//		auth.inMemoryAuthentication().withUser("xi").password(passwordEncoder().encode("123")).roles("ADMIN");
+//		auth.inMemoryAuthentication().withUser("jing").password(passwordEncoder().encode("234")).roles("EMPLOYEE");
 
 		auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
 		
@@ -54,6 +64,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		System.out.println("passwordEncoder()");
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		
+		return super.authenticationManager();
 	}
 
 }
---
+++ boot_intro/src/main/java/com/srivasavi/helper/JWTAuthenticationFilter.java
@@ -0,0 +1,123 @@
+package com.srivasavi.helper;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.srivasavi.service.CustomUserDetailsService;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+	
+	@Autowired
+	JWTHelper jwtHelper;
+	
+	@Autowired
+	CustomUserDetailsService customUserDetailsService;
+
+	@Override
+	protected void doFilterInternal( HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		System.out.println("insdie first of Filter...");
+		
+		
+		String tokenHeader =  request.getHeader("Authorization");
+		System.out.println("token header is "+tokenHeader);
+		
+		String token = null;
+		String username = null;
+		
+		System.out.println("inside filter...");
+		
+		
+		if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+
+			token = tokenHeader.substring(7);
+
+			username = jwtHelper.extractUsername(token);
+			System.out.println("Username is after token decrypt "+username);
+
+			UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+			System.out.println("Userdetails after loading in filter "+userDetails);
+
+			if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+						userDetails, null, userDetails.getAuthorities());
+
+				usernamePasswordAuthenticationToken
+						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			System.out.println("inside filter if...");
+			} else {
+				System.out.println("insdie else of filter");
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+	
+	
+}
+
+
+//@Component
+//public class JWTAuthenticationFilter extends OncePerRequestFilter{
+//	
+//	@Autowired
+//	CustomUserDetailsService customUserDetailsService;
+//	
+//	@Autowired
+//	JWTHelper jwtHelper;
+//
+//	@Override
+//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//			throws ServletException, IOException {
+//		
+//		System.out.println("inside JWTAuthenticationFilter...");
+//		String tokenHeader = request.getHeader("Authorization");
+//		String token = null;
+//		String username = null;
+//
+//		if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+//
+//			token = tokenHeader.substring(7);
+//
+//			username = jwtHelper.extractUsername(token);
+//
+//			UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+//
+//			if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//
+//				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+//						userDetails, null, userDetails.getAuthorities());
+//
+//				usernamePasswordAuthenticationToken
+//						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//
+//				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+//			} else {
+//				System.out.println();
+//			}
+//			
+//		}
+//		filterChain.doFilter(request, response);
+//		
+//	}
+//	
+//	
+//
+//}
